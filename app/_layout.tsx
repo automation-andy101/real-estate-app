5401d7d619518963cff5b5e840d01757445e21c7
automation-andy101 <automation.andy101@gmail.com>
2025-07-08 21:58:50 +0100

    Started adding auth integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,34 @@
 import { useFonts } from "expo-font";
-import { SplashScreen, Stack } from "expo-router";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
-import "./global.css";
 
+import GlobalProvider from "@/lib/global-provider";
+import "./global.css";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
-    "Rubik-Bold": require('../assets/fonts/Rubik-Bold.ttf'),
-    "Rubik-ExtraBold": require('../assets/fonts/Rubik-ExtraBold.ttf'),
-    "Rubik-Light": require('../assets/fonts/Rubik-Light.ttf'),
-    "Rubik-Medium": require('../assets/fonts/Rubik-Medium.ttf'),
-    "Rubik-Regular": require('../assets/fonts/Rubik-Regular.ttf'),
-    "Rubik-SemiBold": require('../assets/fonts/Rubik-SemiBold.ttf'),
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
   });
-
 
   useEffect(() => {
     if (fontsLoaded) {
       SplashScreen.hideAsync();
     }
-  }, [fontsLoaded])
+  }, [fontsLoaded]);
 
+  if (!fontsLoaded) {
+    return null;
+  }
 
-  if (!fontsLoaded) return null;
-
-
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <GlobalProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </GlobalProvider>
+  );
 }
